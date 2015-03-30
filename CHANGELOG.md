87237da10ebfe05568f50184bf7dc99875251d72
Ryo Nakamura <r7kamura@gmail.com>
2015-03-30 11:09:00 +0900

    v0.0.2

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+## 0.0.2
+- Rescue all Exception (e.g. SyntaxError)
+
+## 0.0.1
+- 1st Release :tada:
--- lib/ruboty/ruby/version.rb
+++ lib/ruboty/ruby/version.rb
@@ -1,5 +1,5 @@
 module Ruboty
   module Ruby
-    VERSION = "0.0.1"
+    VERSION = "0.0.2"
   end
 end
